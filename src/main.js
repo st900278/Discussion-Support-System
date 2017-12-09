2c0237c63e576b790760184589ff1edae21f9b6e
Oberon <st900278@gmail.com>
2017-12-10 07:44:49 +0800

    setup vue

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-require("./vue/vue-main.js");
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import temp from "./templates/main.vue";
+Vue.use(Vuetify);
+console.log(temp);
+
+new Vue({
+    el: '#app',
+    render: h => h(temp)
+});
